13caa3599499e9170eb552d9ebbec4e76673f7f7
Jeff <jeffreyrlambert@gmail.com>
2021-08-27 17:28:43 -0600

    JRL: Adds insert_letter_into_sequence function and get_permutations function.  get_permutations function is a recursive function!"

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -1,7 +1,28 @@
 # Problem Set 4A
-# Name: <your name here>
+# Name: Jeff lambert
 # Collaborators:
 # Time Spent: x:xx
+
+
+def insert_letter_into_sequence(sequence, letter):
+    """
+    Returns a list with letter inserted into all possible parts of sequence
+    Args:
+        sequence (str): Sequence where letter will be inserted
+        letter (str): Letter to be inserted into sequence
+
+    Returns:
+        list: Returns list of sequence with letters inserted in various positions
+    """
+    sequence_list = list(sequence)
+    sequence_length = len(sequence) + 1
+    output_list = []
+    for i in range(sequence_length):
+        new_list = sequence_list[:]
+        new_list.insert(i, letter)
+        output_list.append(''.join(new_list))
+    return output_list
+
 
 def get_permutations(sequence):
     '''
@@ -23,7 +44,29 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    try:
+
+        seq_length = len(sequence)
+        seq_list = list(sequence)
+
+        # Check base case where sequence length is 1
+        if seq_length <= 1:
+            return seq_list
+        else:
+            # Held out letter will be inserted into results from previous iteration of function
+            previous_run = get_permutations(sequence[1:])
+            first_letter = seq_list[0]
+            output_list = []
+
+            # For every permutation of the shorter sequence, insert held out letter into that sequence
+            for seq in previous_run:
+                new_output = insert_letter_into_sequence(seq, first_letter)
+                output_list.extend(new_output)
+
+            return output_list
+
+    except TypeError:
+        print("Sequence must be a string")
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -36,5 +79,26 @@ if __name__ == '__main__':
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    example1 = 'abc'
+    expected1 = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    print('Input:', example1)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example1))
+    print('Test Result:', expected1.sort() == get_permutations(example1).sort())
+    print()
 
+    example2 = ''
+    expected2 = ['']
+    print('Input:', example2)
+    print('Expected Output:', [''])
+    print('Actual Output:', get_permutations(example2))
+    print('Test Result:', expected2.sort() == get_permutations(example2).sort())
+    print()
+
+    example3 = 1
+    expected3 = None
+    print('Input:', example3)
+    print('Expected Output:', None)
+    print('Actual Output:', get_permutations(example3))
+    print('Test Result:', expected3 is None)
+    print()
